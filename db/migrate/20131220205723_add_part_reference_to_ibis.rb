f7043276f7f8c24b1b8416ea668b466e3308cf41
DJ Martin <umdjm@hotmail.com>
2014-01-06 14:11:56 -0500

    migrate fix

--- db/migrate/20131220205723_add_part_reference_to_ibis.rb
+++ db/migrate/20131220205723_add_part_reference_to_ibis.rb
@@ -1,6 +1,6 @@
 class AddPartReferenceToIbis < ActiveRecord::Migration
   def change
     add_column :ibis_data, :part_id, :integer
-    add_index :ibis, :part_id
+    add_index :ibis_data, :part_id
   end
 end
